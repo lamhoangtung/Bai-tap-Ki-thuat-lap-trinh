632e06b201ab9237b6b39eee8c5928aa7e29d61e
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-07-16 08:27:12 +0700

    Fixed some bugs

--- Bai thuc hanh so 2/bai5.cpp
+++ Bai thuc hanh so 2/bai5.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#define DG1 750
+#define DG2 1250
+#define DG3 1750
+#define DG4 3000
+
 using namespace std;
 
 int main(){
@@ -8,16 +13,16 @@ int main(){
   cin >> n;
   long long ans;
   if (n<=100){
-    ans=n*750;
+    ans=n*DG1;
   }
   else if (n>100 && n<=200){
-    ans=100*750+(n-100)*1250;
+    ans=100*DG1+(n-100)*DG2;
   }
   else if (n>200 && n<=300){
-    ans=100*750+100*1250+(n-200)*1750;
+    ans=100*DG1+100*DG2+(n-200)*DG3;
   }
   else{
-    ans=100*750+100*1250+100*1750+(n-300)*3000;
+    ans=100*DG1+100*DG2+100*DG3+(n-300)*DG4;
   }
   cout << "So tien dien ho tren phai tra la " << ans << " VND.\n";
   return 0;

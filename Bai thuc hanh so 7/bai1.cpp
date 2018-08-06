d7e98521b2742c2c5e1e2b29241e18f438bae54e
lamhoangtung <lamhoangtung.vz@gmail.com>
2018-08-06 08:28:46 +0700

    Perfected some code

--- Bai thuc hanh so 7/bai1.cpp
+++ Bai thuc hanh so 7/bai1.cpp
@@ -3,12 +3,32 @@
 
 using namespace std;
 
-int main(){
-  ifstream InputFile("FIRSTFILE.txt");
+void writeFile(string filename){
+  ofstream file(filename, ios::out);
+  file << "Probleam name: exp1" << endl;
+  file << "Maximize" << endl;
+  file << "obj:\t\t\t x1 + 2 x2 + 3 x3 + x4" << endl;
+  file << "Subject To" << endl;
+  file << "\t c1: x2 - 3.5 x4 = 0" << endl;
+  file << "Bounds" << endl;
+  file << "\t 0 <= x1 <= 40" << endl;
+  file << "General" << endl;
+  file << "\t x4" << endl;
+  file << "End";
+  file.close();
+}
+
+void readFile(string filename){
+  ifstream file(filename);
   string s;
-  while (getline(InputFile,s)){
+  while (getline(file,s)){
     cout << s << endl;
   }
-  InputFile.close();
+  file.close();
+}
+
+int main(){
+  writeFile("FIRSTFILE.txt");
+  readFile("FIRSTFILE.txt");
   return 0;
 }
--- Bai thuc hanh so 7/bai3.cpp
+++ Bai thuc hanh so 7/bai3.cpp
@@ -3,34 +3,42 @@
 
 using namespace std;
 
-int main(){
+void create(string filename){
   int n,m;
   cout << "Nhap n: ";
   cin >> n;
   cout << "Nhap m: ";
   cin >> m;
-  ofstream ofile;
-  ofile.open("MATRIX.txt");
-  ofile << n << " " << m << endl;
+  ofstream file("MATRIX.txt",ios::out);
+  file << n << " " << m << endl;
   for (int i=0;i<n;i++){
     for (int j=0;j<m;j++){
       cout << "Nhap a[" << i+1 << "," << j+1 << "]: ";
       int temp;
       cin >> temp;
-      ofile << temp << " ";
+      file << temp << " ";
     }
-    ofile << endl;
+    file << endl;
   }
-  ofile.close();
+  file.close();
+}
 
-  int a[1000][1000];
-  ifstream ifile("MATRIX.txt");
-  ifile >> n >> m;
+void read(string filename, int **&a, int &n, int &m){
+  ifstream file(filename);
+  file >> n >> m;
+  a = new int*[n];
+  for (int i=0;i<n;i++){
+    a[i] = new int [m];
+  }
   for (int i=0;i<n;i++){
     for (int j=0;j<m;j++){
-      ifile >> a[i][j];
+      file >> a[i][j];
     }
   }
+  file.close();
+}
+
+void print(int **a, int n, int m){
   cout << "Mang hai chieu da nhap la: " << endl;
   for (int i=0;i<n;i++){
     for (int j=0;j<m;j++){
@@ -38,5 +46,12 @@ int main(){
     }
     cout << endl;
   }
+}
+
+int main(){
+  int n,m,**a;
+  create("MATRIX.txt");
+  read("MATRIX.txt",a,n,m);
+  print(a,n,m);
   return 0;
 }
